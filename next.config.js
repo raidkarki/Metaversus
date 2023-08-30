93aa93bc38ed8c60cd651706ce80f124a913f5b8
A7MED Raid <49642513+raidkarki@users.noreply.github.com>
2023-08-30 19:44:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: false,
+    appDir: true,
   },
-  extends: [
-    //...
-    'plugin:@next/next/recommended',
-  ],
+ 
   
 };
 
